13693a7fbbb92cb3b393d759f445daf2d6504f2d
Rain <448130604@qq.com>
2022-11-07 18:45:14 +0800

    抽离功能，面向接口

---
+++ db.js
@@ -0,0 +1,35 @@
+const homedir = require('os').homedir() //home目录
+const home = process.env.HOME || homedir  //优先home变量
+const fs = require('fs')
+const p = require('path')  //path用来拼路径，防止系统不一致
+const dbPath = p.join(home, '.todo')
+
+const db = {
+  read(path = dbPath) {
+    return new Promise((resolve, reject) => {
+      fs.readFile(dbPath, {flag: 'a+'}, (err, data) => {  //参数多看文档，a+意思是读取并写，且无目录时创建目录
+        if (err) {
+          return reject(err)
+        }
+        let list
+        try {
+          list = JSON.parse(data.toString())
+        } catch (err2) {
+          list = []
+        }
+        resolve(list)
+      })
+    })
+  },
+  write(list, path = dbPath) {
+    return new Promise(((resolve, reject) => {
+      const string = JSON.stringify(list)
+      fs.writeFile(dbPath, string + '\n', (err) => {
+        if (err) {return reject(err)}
+        resolve()
+      })
+    }))
+  }
+}
+
+module.exports = db
--- index.js
+++ index.js
@@ -3,30 +3,13 @@ const home = process.env.HOME || homedir  //优先home变量
 const fs = require('fs')
 const p = require('path')  //path用来拼路径，防止系统不一致
 const dbPath = p.join(home, '.todo')
+const db = require('./db.js')
 
-module.exports.add = (title) => {
+module.exports.add = async (title) => {
   // 读取之前的任务
+  const list = await db.read()
   // 往里面添加一个 title 任务
+  list.push({title,done:false})
   // 存储任务到文件
-  fs.readFile(dbPath, {flag: 'a+'}, (err, data) => {  //参数多看文档，a+意思是读取并写，且无目录时创建目录
-    if (err) {
-      console.log(err)
-    } else {
-      let list
-      try {
-        list = JSON.parse(data.toString())
-      } catch (err2) {
-        list = []
-      }
-      const task = {
-        title: title,
-        done: false
-      }
-      list.push(task)
-      const string = JSON.stringify(list)
-      fs.writeFile(dbPath, string + '\n', (err3) => {
-        if (err3) {console.log(err3)}
-      })
-    }
-  })
+  await db.write(list)
 }
