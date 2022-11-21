420659d8b78ad011b95651daff4a8419a73bba09
Rain <448130604@qq.com>
2022-11-21 17:22:11 +0800

    发布

--- cli.js
+++ cli.js
@@ -1,24 +1,26 @@
+#!/usr/bin/env node
 const program = require('commander')
 const api = require('./index')
+const pkg = require('./package.json')
 
 program  //选项
-  .option('-x, --xxx', 'xxx')
+  .version(pkg.version)
 program  //命令
   .command('add')
   .description('add a task')
   .action((...args) => {
     const words = args.slice(0, -1).join(' ')
-    api.add(words).then(() => {console.log('添加成功')},()=>{console.log('添加失败')})
+    api.add(words).then(() => {console.log('添加成功')}, () => {console.log('添加失败')})
   })
 program  //命令
   .command('clear')
   .description('clear all tasks')
   .action(() => {
-    api.clear().then(() => {console.log('清除成功')},()=>{console.log('清除失败')})
+    api.clear().then(() => {console.log('清除成功')}, () => {console.log('清除失败')})
   })
 
 program.parse(process.argv)
 
-if(process.argv.length ===2 ){
+if (process.argv.length === 2) {
   void api.showAll()
 }
